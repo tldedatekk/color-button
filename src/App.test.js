000aecbff55b6442104c1312ddef246e193f8542
tldedatekk <tldedatekk@gmail.com>
2022-02-04 16:51:58 +1100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct intitial color', () => {
+ render (<App />);
+ //find an element with a role of button and text of "change to blue"
+ const colorButton = screen.getByRole('button', {name: 'Change to blue'})
+ //expect the background color to be red
+ expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+});
+
+test('button has correct initial text', () =>{
+
+});
+
+test('button turns blue when clicked',() => {
+
 });
